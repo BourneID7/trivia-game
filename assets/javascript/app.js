def0a52735dc87d56afbb83af40646311a7691f6
Jason Bourne <m.bourne@wi.rr.com>
2019-04-29 07:32:35 -0500

    added trivia questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,10 +7,7 @@ var timeRemaining = 30;
 var questions = ["#question-1", "#question-2", "#question-3", "#question-4", "#question-5", "#question-6", "#question-7", "#question-8", "#question-9", "#question-10"];
 
 // variable to hold correct answers
-var correctAnswers = ["calatrava"];
-
-// determine if a radio button has been checked
-var checked
+var correctAnswers = ["calatrava", "clock2", "70sShow", "maier", "bastille", "1903", "joanArc", "brickColor", "rock", "lesPaul"];
 
 // variable to hold user answers
 var userAnswers = [];
@@ -19,17 +16,19 @@ var userAnswers = [];
 var showQuestion;
 
 // count keeps track of the index of the currently displaying question.
-var count = 0;
+var count;
 
 //  Variable that holds setInterval that runs the countdown
 var intervalId;
 
-// display messages for correct, incorrect or unanswered questions
-var displayCorrect;
-var displayWrong;
-var displayTimeout;
+// variable to record total correct, incorrect or unanswered questions
+var totalCorrect = 0;
+var totalWrong = 0;
+var totalTimeout = 0;
 
 $(document).ready(function(){
+
+    var myTimer = setInterval(startTimer, 30000);
 
     // function to start timer
     function startTimer() {
@@ -38,14 +37,14 @@ $(document).ready(function(){
             timeRemaining -= 1;
             if(timeRemaining <= 0){
                 clearInterval(intervalId);
-                $("#timer").text("Time's up!");
+                $("#timer").text("Time's up! Next question.");
             }
         }, 1000);
     }
     // function to stop timer
     function stopTimer() {
-        clearInterval(intervalId);
-      }
+        clearInterval(myTimer);
+    }
     
 
     // start game
@@ -55,23 +54,42 @@ $(document).ready(function(){
         // function to load question & answer choices
         function startQuestions() {
 
-            // Show the first question in #question-1 div.
-            $("#question-1").addClass(".show");
+        // Show the first question in #question-1 div.
+        $("#question-1").addClass(".show");
+
         }
         startQuestions();
         startTimer();
         function userChoice() {
             $("input[type='radio']").click(function(){
                 var radioValue = $("input[name='question-1']:checked").val();
+                stopTimer();
+
                 if(radioValue){
                     userAnswers.push(radioValue);
                     console.log("user choice is " + radioValue);
                     console.log(userAnswers);
-                    stopTimer();
                 }
             })
         }
         userChoice();
+        // on click answer choice determine correct/incorrect
+        // if (userAnswers[count] === correctAnswers[count]) {
+        //     $("#messageCorrect").text("You're right!");
+        //     totalCorrect++;
+        //     console.log("total correct answers: " + totalCorrect)
+
+        // } else if (userAnswers[count] !== correctAnswers[count]) {
+        //     $("#messageWrong").text("Sorry, wrong answer. The correct answer is " + correctAnswers[count]);
+        //     totalWrong++;
+        //     console.log("total wrong answers: " + totalWrong);
+        // } else if (timeRemaining === 0) {
+        //     $("#messageTimeout").text("Sorry, you're out of time. The correct answer is " + correctAnswers[count]);
+        //     totalTimeout++;
+        //     console.log("total unanswered questions: " + totalTimeout);
+        // }
+        
+
     })
 
 
@@ -84,13 +102,10 @@ $(document).ready(function(){
         setTimeout(showQuestion, 30000);
     
         // If the count is the same as the length of the image array, display end of game screen.
-        if (count === questions.length) {
-        count = 0;
-        }
+       
     }
 
 
-    // on click answer choice determine correct/incorrect
 
     // if correct display correct message & load next question. Restart timer.
 
